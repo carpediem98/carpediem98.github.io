3611321b667e29d6a9ddad726397703d77daa461
Chunjun Jia <jiachunjun2013@gmail.com>
2019-09-26 00:11:37 -0400

    Updates

--- .cache/api-runner-browser-plugins.js
+++
@@ -1,7 +0,0 @@
-module.exports = [{
-      plugin: require('/Users/pauljia/Documents/GitHub/carpediem98.github.io/node_modules/gatsby-plugin-offline/gatsby-browser.js'),
-      options: {"plugins":[]},
-    },{
-      plugin: require('/Users/pauljia/Documents/GitHub/carpediem98.github.io/gatsby-browser.js'),
-      options: {"plugins":[]},
-    }]
